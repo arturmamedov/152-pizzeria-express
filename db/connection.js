8b63a41175f364e0fa22d8c8de48dcf678fd64aa
Artur Mamedov <arturmamedov1993@gmail.com>
2026-03-13 13:51:55 +0100

    feat (.env): Created .env file configuration, added it to server run commands and to mysql configuration file

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: ''
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 connection.connect((err) => {
